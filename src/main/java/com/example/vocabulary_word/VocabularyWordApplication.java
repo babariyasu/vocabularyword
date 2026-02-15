3edfd2598f46123fa4833d27d51c82beebbcc4af
babariyasu <xiaojiamachang@gmail.com>
2026-02-15 23:16:54 +0900

    新規作成

---
+++ src/main/java/com/example/vocabulary_word/VocabularyWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.vocabulary_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyWordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vocabulary_word/VocabularyWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vocabulary_word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
